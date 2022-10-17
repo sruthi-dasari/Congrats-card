398d58cef369604d3e22714112325b5c8adb4c2c
sruthi <sruthi@example.com>
2022-10-17 14:49:16 +0000

    output not getting displayed but all test cases passed

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const element = (
+    <div className="bg-container">
+    <h1 className = "title">Congratulations</h1>
+    <div className = "card">
+        
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"/>
+        <h1 className = "profile-name">Kiran V</h1>
+        <p className = "profile-details">Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"/>
+    </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
